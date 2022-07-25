5d0b9f962d9e4aa5c01f355e0c8c9739648e62c9
Muhammad Sheharyar Butt <56956294+shehari007@users.noreply.github.com>
2022-07-25 19:41:54 +0300

    axios api yukledim + database baglanma icin daha calisiyorum

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -31,7 +31,7 @@ const footer = () => {
             </p>
             <CDBBox flex="column" display="flex" style={{ cursor: 'pointer', padding: '0' }}>
               <CDBFooterLink href="/anasayfa">Ana Sayfa</CDBFooterLink>
-              <CDBFooterLink href="./components/Tabs/hakkimizda.js">Hakkimizda</CDBFooterLink>
+              <CDBFooterLink href="/hakkinda">Hakkimizda</CDBFooterLink>
               <CDBFooterLink href="/iletesim">Iletesim</CDBFooterLink>
               <CDBFooterLink href="/yasaluyari">Yasal Uyari</CDBFooterLink>
             </CDBBox>
--- src/pages/ogrenci.js
+++ src/pages/ogrenci.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './ogrenci.css';
+import Database from '../components/Database/database';
 
 
 const ogrenci = () => {
   return (
-         <div>ogrenci sayfasi</div>    
+         <><div>ogrenci sayfasi</div>
+         <Database /></>
   )
 }
 
