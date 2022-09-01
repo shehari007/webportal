74f53d1919c721e35de9bab357ea839fc80876a4
Muhammad Sheharyar Butt <56956294+shehari007@users.noreply.github.com>
2022-09-01 08:11:03 +0300

    php updated

--- src/php_api_configs/auth.php
+++ src/php_api_configs/auth.php
@@ -1,8 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=utf-8');
-error_reporting(E_ERROR | E_PARSE);
+//error_reporting(E_ERROR | E_PARSE);
 include 'config_db.php';
+//echo $_POST["action"];
 
 if ($_POST["action"] == "register") {
     $sql = 'INSERT INTO users (username, email, password) values ("' . $_POST["username"] . '","' . $_POST["email"] . '","' . $_POST["password"] . '")';
@@ -11,12 +12,13 @@ if ($_POST["action"] == "register") {
 }
 
 if ($_POST["action"] == "login") {
-    $sql = 'SELECT  email, password FROM users WHERE email= "' . $_POST["email"] . '" AND password= "' . $_POST["password"] . '"';
+    
+    $sql = 'SELECT id from users where email= "' . $_POST["email"] . '" AND password= "' . $_POST["password"] . '"';
     $result = $conn->query($sql);
-    $jsonArray  = $result;
+    $jsonArray  = $result->num_rows;
 }
 
-if ($_GET["action"] == "list") {
+if ($_POST["action"] == "list") {
     $sql = "SELECT  username, password, email FROM users";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
